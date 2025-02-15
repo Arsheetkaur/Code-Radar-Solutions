7476c5eb8c67322f134d96960c77676537060881
Arsheet Kaur <arsheet0946.becse24@chitkara.edu.in>
2025-02-15 12:00:34 +0530

    Solution to Logical AND Operation

---
+++ Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
@@ -0,0 +1,14 @@
+// Your code here...
+# incldue <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (a,b > 0) {
+        printf("True \n");
+    }
+    else {
+        printf("False \n");
+    }
+    return 0;
+}
